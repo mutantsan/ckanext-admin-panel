a213b3eddab0a5fe65b2fc0cdd227426ba7bdf03
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-07-30 22:07:01 +0300

    chore: switch to blueprints blanket

--- ckanext/admin_panel/views/__init__.py
+++ ckanext/admin_panel/views/__init__.py
@@ -2,6 +2,8 @@ from ckanext.admin_panel.views.basic import ap_basic
 from ckanext.admin_panel.views.config import ap_config_list
 from ckanext.admin_panel.views.user import ap_user
 
-
-def get_blueprints():
-    return [ap_basic, ap_config_list, ap_user]
+__all__ = [
+    "ap_basic",
+    "ap_config_list",
+    "ap_user",
+]
