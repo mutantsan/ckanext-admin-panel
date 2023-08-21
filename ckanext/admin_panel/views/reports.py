8280ab324edcff64ad68c99f349ab6dcc533c532
Oleksandr Cherniavskyi <kvaqich@gmail.com>
2023-08-21 15:36:17 +0300

    feature: create a  snippet

--- ckanext/admin_panel/views/reports.py
+++ ckanext/admin_panel/views/reports.py
@@ -11,18 +11,35 @@ from ckanext.admin_panel.utils import ap_before_request
 from ckanext.admin_panel.model import ApLogs
 from ckan.lib.helpers import Page
 
+from ckanext.admin_panel.helpers import ap_table_column as ap_column
+
 ap_report = Blueprint("ap_report", __name__, url_prefix="/admin-panel")
 ap_report.before_request(ap_before_request)
 
 
 class ReportLogsView(MethodView):
     def get(self) -> Union[str, Response]:
-        log_items = ApLogs.all()
+        self.q = tk.request.args.get("q", "").strip()
+        self.order_by = tk.request.args.get("order_by", "name")
+        self.sort = tk.request.args.get("sort", "desc")
+        self.type = tk.request.args.get("type", "")
+        self.level = tk.request.args.get("level", "")
 
         return tk.render(
             "admin_panel/config/reports/logs.html",
-            extra_vars={"page": self._get_pager(log_items)},
+            extra_vars=self._prepare_data_dict(),
         )
+
+    def _prepare_data_dict(self) -> dict[str, Any]:
+        return {
+            "page": self._get_pager(ApLogs.all()),
+            "columns": self._get_table_columns(),
+            "q": self.q,
+            "order_by": self.order_by,
+            "sort": self.sort,
+            "state": self.type,
+            "role": self.level,
+        }
 
     def _get_pager(self, log_list: list[dict[str, Any]]) -> Page:
         page_number = tk.h.get_page_number(tk.request.args)
@@ -36,6 +53,15 @@ class ReportLogsView(MethodView):
             item_count=len(log_list),
             items_per_page=limit,
         )
+
+    def _get_table_columns(self) -> list[dict[str, Any]]:
+        return [
+            ap_column("name", width="10%"),
+            ap_column("path", width="20%"),
+            ap_column("level",width="5%"),
+            ap_column("timestamp", type_="date", width="10%"),
+            ap_column("message", sortable=False, width="55%"),
+        ]
 
     def post(self) -> Response:
         return tk.redirect_to("ap_user.list")
