790277130f6c8ef1b27e0b868dc4ad5cd5668238
Oleksandr Cherniavskyi <kvaqich@gmail.com>
2023-08-29 17:15:46 +0300

    feature: basic implementation of content page, FINISH

--- ckanext/admin_panel/views/content.py
+++ ckanext/admin_panel/views/content.py
@@ -8,18 +8,17 @@ from flask import Blueprint, Response
 from flask.views import MethodView
 from typing_extensions import TypeAlias
 
-import ckan.lib.navl.dictization_functions as df
 import ckan.logic as logic
 import ckan.plugins.toolkit as tk
 from ckan import model, types
 from ckan.lib.helpers import Page
 
-from ckanext.admin_panel.utils import ap_before_request
+import ckanext.admin_panel.utils as ap_utils
 
 ContentList: TypeAlias = "list[dict[str, Any]]"
 
 ap_content = Blueprint("ap_content", __name__, url_prefix="/admin-panel")
-ap_content.before_request(ap_before_request)
+ap_content.before_request(ap_utils.ap_before_request)
 
 log = logging.getLogger(__name__)
 
@@ -86,29 +85,51 @@ class ContentListView(MethodView):
             group["metadata_modified"] = group["created"]
             group["notes"] = group["description"]
 
-        return result_pkgs + result_orgs + [grp for grp in result_groups]
+        return (
+            result_pkgs
+            + get_deleted_datasets()
+            + result_orgs
+            + get_deleted_orgs()
+            + result_groups
+            + get_deleted_groups()
+        )
 
     def post(self) -> Response:
         bulk_action = tk.request.form.get("bulk-action")
-        content_ids = tk.request.form.getlist("entity_id")
-
-        if not bulk_action or not content_ids:
-            return tk.redirect_to("ap_content.list")
-
-        action_func = self._get_bulk_action(bulk_action)
+        content_ids_to_types = tk.request.form.getlist("entity_id")
+        action_func = self._get_bulk_action(bulk_action) if bulk_action else None
 
         if not action_func:
             tk.h.flash_error(tk._("The bulk action is not implemented"))
             return tk.redirect_to("ap_content.list")
 
-        action_func(content_ids)
-
-        tk.h.flash_success(tk._("Done."))
+        for entity_type, entity_ids in self._group_entity_ids_by_types(
+            content_ids_to_types
+        ).items():
+            if action_func(entity_ids, entity_type):
+                tk.h.flash_success(tk._("Done."))
 
         return tk.redirect_to("ap_content.list")
 
+    def _group_entity_ids_by_types(
+        self, ids_to_types: list[str]
+    ) -> dict[str, list[str]]:
+        result = {}
+
+        for row in ids_to_types:
+            try:
+                entity_id, entity_type = row.split("|")
+            except ValueError:
+                continue
+
+            result.setdefault(entity_type, [])
+            result[entity_type].append(entity_id)
+
+        return result
+
     def _get_pager(self, content_list: ContentList) -> Page:
         page_number = tk.h.get_page_number(tk.request.args)
+        # TODO add config
         limit: int = 20
 
         return Page(
@@ -124,7 +145,9 @@ class ContentListView(MethodView):
             tk.h.ap_table_column("title", "Title", width="10%"),
             tk.h.ap_table_column("notes", "Notes", width="30%", sortable=False),
             tk.h.ap_table_column("type", "Type", width="10%", sortable=False),
-            tk.h.ap_table_column("creator_user_id", "Author", type_="user_link", width="10%"),
+            tk.h.ap_table_column(
+                "creator_user_id", "Author", type_="user_link", width="10%"
+            ),
             tk.h.ap_table_column("state", "State", width="10%"),
             tk.h.ap_table_column(
                 "metadata_created", "Create at", type_="date", width="10%"
@@ -156,48 +179,75 @@ class ContentListView(MethodView):
         return [
             {
                 "value": "1",
-                "text": tk._("Purge selected entities(s)"),
+                "text": tk._("Restore selected entities(s)"),
             },
             {
                 "value": "2",
-                "text": tk._("Restore selected user(s)"),
+                "text": tk._("Delete selected entities(s)"),
+            },
+            {
+                "value": "3",
+                "text": tk._("Purge selected entities(s)"),
             },
         ]
 
-    def _get_bulk_action(self, value: str) -> Callable[[list[str]], None] | None:
+    def _get_bulk_action(self, value: str) -> Callable[[list[str], str], bool] | None:
         return {
-            # "1": partial(self._change_user_role, is_sysadmin=True),
-            # "2": partial(self._change_user_role, is_sysadmin=False),
-            # "3": partial(self._change_user_state, is_active=False),
-            # "4": partial(self._change_user_state, is_active=True),
+            "1": partial(self._change_entities_state, is_active=True),
+            "2": partial(self._change_entities_state, is_active=False),
+            "3": partial(self._purge_entities),
         }.get(value)
 
-    # @staticmethod
-    # def _change_user_role(
-    #     user_ids: list[str], is_sysadmin: Optional[bool] = False
-    # ) -> None:
-    #     for user_id in user_ids:
-    #         try:
-    #             logic.get_action("user_patch")(
-    #                 {"ignore_auth": True}, {"id": user_id, "sysadmin": is_sysadmin}
-    #             )
-    #         except tk.ObjectNotFound:
-    #             pass
+    @staticmethod
+    def _change_entities_state(
+        entity_ids: list[str], entity_type: str, is_active: Optional[bool] = False
+    ) -> bool:
+        state = model.State.ACTIVE if is_active else model.State.DELETED
+        actions = {
+            "dataset": "package_patch",
+            "organization": "organization_patch",
+            "group": "group_patch",
+        }
+        action = actions.get(entity_type)
 
-    # @staticmethod
-    # def _change_user_state(
-    #     user_ids: list[str], is_active: Optional[bool] = False
-    # ) -> None:
-    #     state = model.State.ACTIVE if is_active else model.State.DELETED
+        if not action:
+            tk.h.flash_error(f"Changing {entity_type} entity state isn't supported")
+            return False
 
-    #     for user_id in user_ids:
-    #         try:
-    #             logic.get_action("user_patch")(
-    #                 {"ignore_auth": True},
-    #                 {"id": user_id, "state": state},
-    #             )
-    #         except tk.ObjectNotFound:
-    #             pass
+        for entity_id in entity_ids:
+            try:
+                logic.get_action(action)(
+                    {"ignore_auth": True},
+                    {"id": entity_id, "state": state},
+                )
+            except tk.ObjectNotFound:
+                pass
+
+        return True
+
+    @staticmethod
+    def _purge_entities(entity_ids: list[str], entity_type: str) -> bool:
+        actions = {
+            "dataset": "dataset_purge",
+            "organization": "organization_purge",
+            "group": "group_purge",
+        }
+        action = actions.get(entity_type)
+
+        if not action:
+            tk.h.flash_error(f"Purging {entity_type} entity isn't supported")
+            return False
+
+        for entity_id in entity_ids:
+            try:
+                logic.get_action(action)(
+                    {"ignore_auth": True},
+                    {"id": entity_id},
+                )
+            except tk.ObjectNotFound:
+                pass
+
+        return True
 
     def _filter_by_state(self, content_list: ContentList) -> ContentList:
         if self.state == model.State.DELETED:
@@ -238,6 +288,54 @@ class ContentListView(MethodView):
             key=lambda x: x.get(self.order_by, ""),
             reverse=self.sort == "desc",
         )
+
+
+def get_deleted_datasets() -> list[dict[str, Any]]:
+    """Return a list of deleted datasets, either from DB or from search index"""
+    return (
+        _get_deleted_datasets_from_db()
+        if tk.config["ckan.search.remove_deleted_packages"]
+        else _get_deleted_datasets_from_search_index()
+    )
+
+
+def _get_deleted_datasets_from_db() -> list[dict[str, Any]]:
+    return [
+        package.as_dict()
+        for package in model.Session.query(model.Package)
+        .filter_by(state=model.State.DELETED)
+        .all()
+    ]
+
+
+def _get_deleted_datasets_from_search_index() -> list[dict[str, Any]]:
+    results = tk.get_action("package_search")(
+        {"ignore_auth": True},
+        {
+            "fq": "+state:deleted",
+            "include_private": True,
+        },
+    )
+
+    return results["results"]
+
+
+def get_deleted_orgs() -> list[dict[str, Any]]:
+    return [
+        org.as_dict()
+        for org in model.Session.query(model.Group)
+        .filter_by(state=model.State.DELETED, is_organization=True)
+        .all()
+    ]
+
+
+def get_deleted_groups() -> list[dict[str, Any]]:
+    return [
+        group.as_dict()
+        for group in model.Session.query(model.Group).filter_by(
+            state=model.State.DELETED, is_organization=False
+        )
+    ]
 
 
 class ContentProxyView(MethodView):
