330a1eeef2e927d465f1dc494fe06b5b75067902
mutantsan <kvaqich@gmail.com>
2024-01-29 14:45:26 +0200

    update: add ap-bulk-check reinit after htmx request

--- ckanext/ap_main/assets/js/ap-bulk-check.js
+++ ckanext/ap_main/assets/js/ap-bulk-check.js
@@ -8,10 +8,23 @@ ckan.module("ap-bulk-check", function ($) {
             $.proxyAll(this, /_on/);
 
             $(this.el).click(this._onClick)
+
+            document.addEventListener('htmx:afterRequest', this._onAfterRequest);
         },
 
         _onClick(e) {
             $(this.options.selector).prop("checked", $(e.target).prop("checked"));
         },
+
+        /**
+         * Reinit bulk check script after HTMX request
+         *
+         * @param {Event} evt
+         */
+        _onAfterRequest: function (evt) {
+            $('[data-module*="ap-bulk-check"]').each((_, el) => {
+                $(el).click(this._onClick);
+            })
+        }
     };
 });
